38b02aab6f61ae08964bb49ed206c5a715bcc619
Vesdi4 <111139167+Vesdi4@users.noreply.github.com>
2026-02-26 22:51:07 +0200

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/_deps/nontype_functional-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/Documents/Level-Notes-gd/out/build/x64-Debug/_deps/nontype_functional-src
-# Build directory: C:/Users/Admin/Documents/Level-Notes-gd/out/build/x64-Debug/_deps/nontype_functional-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
